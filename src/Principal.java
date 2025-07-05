e45f4e3ec137e70b48c2f5e12c6efc268efee405
Gilvaneide Medeiros <gilvaneide.m.bertaccini@gmail.com>
2025-07-05 00:24:15 -0300

    Commit inicial.

---
+++ src/Principal.java
@@ -0,0 +1,50 @@
+import Compra.Compra;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("\nDigite o limite do cartão:");
+        Double limite =leitura.nextDouble();
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
+
+        int sair =1;
+        while(sair != 0) {
+            System.out.println("Digite o nome do produto: ");
+            String descricao = leitura.next();
+
+            System.out.println("Digite o valor do produto: ");
+            Double valor = leitura.nextDouble();
+
+            Compra compra = new Compra(descricao, valor);
+            boolean compraRealizada = cartao.lancaCompra(compra);
+
+            if(compraRealizada) {
+                System.out.println("Compra realizada com sucesso!");
+                System.out.println("Saldo atual: " + cartao.getSaldo());
+
+                System.out.println("Deseja realizar outra compra?");
+                System.out.println("1 - Sim");
+                System.out.println("0 - Não");
+                sair = leitura.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente!");
+                sair = 0;
+            }
+        }
+
+        System.out.println("==========================================");
+        System.out.println("Compras realizadas:\n");
+        Collections.sort(cartao.getCompras());
+        for(Compra c : cartao.getCompras()) {
+            System.out.println(c.getDescricao() + " - R$" + c.getValor());
+        }
+        System.out.println("==========================================");
+
+        System.out.println("Saldo do cartão: " + cartao.getSaldo());
+
+    }
+}
